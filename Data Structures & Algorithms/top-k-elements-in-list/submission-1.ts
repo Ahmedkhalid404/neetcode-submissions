8add46d51b2a7db682c184fa12893dd0dcdab5c2
Ahmed khaled <116371216+Ahmedkhalid404@users.noreply.github.com>
2026-05-14 19:01:44 +0300

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.ts
@@ -0,0 +1,26 @@
+interface data{
+    el:number;
+    freq:number;
+}
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        let freq = new Map<number, number>();
+        for(const el of nums){
+            const curr: number = freq.get(el) ?? 0;
+            freq.set(el, curr + 1);
+        }
+        let arr : data[] = [];
+        for(let [el, f] of freq){
+            arr.push({
+                el,
+                freq : f
+            });
+        }
+        return arr.sort((a: data, b: data) => b.freq - a.freq).slice(0, k).map((a: data) => a.el);
+    }
+}
